f9c79c0891cffa013faa648aca2061f265f39054
Madera <fealbemadera@gmail.com>
2023-01-16 15:37:18 -0600

    CorrectionBash

--- canscript.sh
+++ canscript.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 x=1
-while [$x -le 50 ]; do
+while [ $x -le 50 ]
+do
 
     cansend vcan0 123#00ffaadeadbeed
     echo "cansend $x times"
-    x=$($x + 1)
+    x=$(( $x + 1 ))
     sleep 5
 
 done
